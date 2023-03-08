ce165d01ae1a30aa25c16d0c3519cf18c932cbed
Priyosmita <priyosh08@gmail.com>
2023-03-08 19:49:35 +0530

    login page update

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,48 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import React, { useState } from "react";
+import { toast } from "react-hot-toast";
+import { Link, useNavigate } from "react-router-dom";
 import InputField from "./InputField";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleChange = (textValue, key) => {
+    if (key === "email") setEmail(textValue);
+    else if (key === "password") setPassword(textValue);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!email || !password) {
+      toast.error("Fill all required fields");
+      return;
+    }
+
+    // https://omniblog.cyclic.app/signup
+    axios
+      .post("https://omniblog.cyclic.app/login", {
+        email,
+        password,
+      })
+      .then((res) => {
+        toast.success("Login Successfull!!");
+        console.log(res.data);
+
+        localStorage.setItem("accessToken", res.data.data["access-token"]);
+        localStorage.setItem("refreshToken", res.data.data["refresh-token"]);
+        localStorage.setItem("userData", JSON.stringify(res.data.data["user"]));
+
+        navigate("/"); 
+      })
+      .catch((err) => {
+        toast.error("Login Failed");
+        console.error(err);
+      });
+  };
+
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="border-2 py-16 px-10 rounded-md">
@@ -13,9 +53,25 @@ const Login = () => {
           <p>Start your 30-day free trial. No credit Card.</p>
         </div>
         <form className="flex flex-col item-center gap-3 mb-5">
-          <InputField type="email" placeholder="Email" />
-          <InputField type="password" placeholder="Password" />
-          <button className="bg-black text-white p-2 rounded-lg mt-5">
+          <InputField
+            value={email}
+            fieldKey={"email"}
+            handleChange={handleChange}
+            type="email"
+            placeholder="Email"
+          />
+          <InputField
+            value={password}
+            fieldKey={"password"}
+            handleChange={handleChange}
+            type="password"
+            placeholder="Password"
+          />
+          <button
+            type="submit"
+            onClick={(e) => handleSubmit(e)}
+            className="bg-black text-white p-2 rounded-lg mt-5"
+          >
             L O G I N
           </button>
         </form>
